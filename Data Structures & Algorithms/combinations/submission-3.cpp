12ef07b57159e82e02f644cb89b978b680eda9bc
Scarlett <lisijia0807@gmail.com>
2026-06-05 15:27:10 -0400

    Add: combinations - submission-3

---
+++ Data Structures & Algorithms/combinations/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    vector<int> path;
+    vector<vector<int>> res;
+    void backtracking(int n, int k, int startidx){
+        if (path.size() == k){
+        	res.push_back(path);
+            return;
+            }
+        for (int i = startidx; i <=n-(k-path.size())+1; i++){
+            path.push_back(i);
+            backtracking(n,k,i+1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n,k,1);
+        return res;
+
+    }
+};
